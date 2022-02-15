72ac31a8ebfbfc8bedb93b64e85b95d130ca5caf
Eduardo Tolino <eduardo.tolino@tolitech.com.br>
2022-02-15 14:47:15 -0300

    adding IEmailService.

---
+++ src/Domain/Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Tolitech.CodeGenerator.Domain.Services
+{
+    public interface IEmailService : IInfrastructureService
+    {
+        string GetTemplate(string filePath, params string[] parameters);
+
+        void Send(string to, string subject, string body, string? cc = null, string? bcc = null, IList<Attachment>? attachments = null);
+    }
+}
