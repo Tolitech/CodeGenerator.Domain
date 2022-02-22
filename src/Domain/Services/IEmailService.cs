c837e71383f25003037a4aacaf2dba539bda4a12
Eduardo Tolino <eduardo.tolino@tolitech.com.br>
2022-02-22 17:39:16 -0300

    nullable reference type.

--- src/Domain/Services/IEmailService.cs
+++ src/Domain/Services/IEmailService.cs
@@ -5,7 +5,7 @@ namespace Tolitech.CodeGenerator.Domain.Services
 {
     public interface IEmailService : IInfrastructureService
     {
-        string GetTemplate(string filePath, params string[] parameters);
+        string GetTemplate(string? filePath, params string[]? parameters);
 
         void Send(string host, int port, string username, string password, string from, string to, string subject, string body, string? cc = null, string? bcc = null, IList<Attachment>? attachments = null);
     }
